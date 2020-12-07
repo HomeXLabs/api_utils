8f85d6a32bd14b8149e672c9448a7d8554ff10d3
Jason Rai <jrai@homex.com>
2020-12-07 10:55:50 -0500

    1.0.0 cleanup (#7)

--- lib/src/api_response.dart
+++ lib/src/api_response.dart
@@ -1,11 +1,19 @@
 import 'package:api_utils/src/status_code.dart';
 
-/// Response from an api, with a typed [data] field
+/// Response from a web api, with a typed [data] field
 class ApiResponse<T> {
+  /// Http status code returned from the server
   final int statusCode;
+
+  /// Response data transformed into a strongly typed object
   final T data;
+
+  /// Only populated when the request fails for some reason. This value will
+  /// contain an error response from the server or exception information if
+  /// the request failed before getting a response.
   final String error;
 
+  /// If the request was successful
   bool get isSuccess => isSuccessStatusCode(statusCode);
 
   ApiResponse(this.statusCode, {this.data, this.error});
